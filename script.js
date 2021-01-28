d28654b9c8ba58a0fb19e3feca16e803afcccb30
Keano Brion <keanobrion@gmail.com>
2021-01-28 15:19:48 +0800

    Add control panel

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 let container = document.querySelector('#container');
+const button=document.querySelector('#buttons');
+const range=document.querySelector('#numberOfBoxes');
+const numberLabel = document.querySelector('#numberLabel');
+const color = document.querySelector('#colorOfBoxes');
+const toggleDrawing = document.querySelector('#toggleDrawing');
+let drawing = 0;
+
+
+
+button.addEventListener('click', clear);
+range.addEventListener('change', sketch);
+range.addEventListener('input', changeLabel);
+window.addEventListener('resize', cssChange);
+
+function changeLabel(){
+    let numberBoxes = document.getElementById("numberOfBoxes").value;
+    numberLabel.textContent = `${numberBoxes} x ${numberBoxes} `;
+}
 
 function sketch(){
-    let x = prompt('How many squares?');
+    container = document.querySelector('#container');
+    let boxes = Array.from(document.querySelectorAll(".boxes"));
+    boxes.forEach(box => container.removeChild(box));
+    let numberBoxes = document.getElementById("numberOfBoxes").value
+    numberLabel.textContent = `${numberBoxes} x ${numberBoxes} `
+    let x = numberBoxes;
     let z= document.getElementById('container').clientWidth;
+    let y= document.getElementById('container').clientHeight
     let rules = [];
 
         //Modifies the third rule on css which is .boxes by updating its width and height;
@@ -12,8 +36,10 @@ function sketch(){
             else if (document.styleSheets[0].rules){
                 rules = document.styleSheets[0].rules;
             }
-                rules[2].style.width = `${z/x}px`;
-                rules[2].style.height = `${z/x}px`;
+                // rules[2].style.width = `${z/x}px`;
+                // rules[2].style.height = `${y/x}px`;
+                rules[2].style.width = `${100/x}%`;
+                rules[2].style.height = `${100/x}%`;
 
         //creates the boxes        
         for (i=1; i<=x; i++){
@@ -26,18 +52,23 @@ function sketch(){
             }
         }
 
-        const boxes = Array.from(document.querySelectorAll(".boxes"));
+        boxes = Array.from(document.querySelectorAll(".boxes"));
         boxes.forEach(box => box.addEventListener('mouseover', hoverMouse));
+       
 }
 
-sketch();
 
 function hoverMouse(e){
-        e.target.classList.add('hover');
+    if(drawing===1){
+    let colorBoxes = document.getElementById("colorOfBoxes").value
+    console.log(colorBoxes);
+    e.target.setAttribute("style", `background-color: ${colorBoxes};`);
+    }
+    else{
+        return;
+    }
 }
 
-const button=document.querySelector('#buttons');
-button.addEventListener('click', clear);
 
 function clear(){
     console.log('hello');
@@ -47,4 +78,46 @@ function clear(){
     sketch();
 }
 
+function cssChange(){
+    let numberBoxes = document.getElementById("numberOfBoxes").value
+    let x = numberBoxes;
+    let z= document.getElementById('container').clientWidth;
+    let y= document.getElementById('container').clientHeight
+    let rules = [];
 
+        //Modifies the third rule on css which is .boxes by updating its width and height;
+            if (document.styleSheets[0].cssRules){
+                rules = document.styleSheets[0].cssRules;
+            }
+            else if (document.styleSheets[0].rules){
+                rules = document.styleSheets[0].rules;
+            }
+                rules[2].style.width = `${z/x}px`;
+                rules[2].style.height = `${y/x}px`;
+}
+
+
+document.addEventListener('keydown', draw);
+
+function draw(event){
+    let x=event.which||event.keyCode;
+    if(x === 68){
+        console.log('pumasok')
+        if(drawing === 0){
+            drawing = 1; 
+            console.log('draw is on')
+            toggleDrawing.textContent = "Drawing is On";
+            toggleDrawing.setAttribute("style", "color:green;");
+        }
+        else if(drawing === 1){
+            drawing = 0;
+            console.log('draw is off')
+            toggleDrawing.textContent = "Drawing is Off";
+            toggleDrawing.setAttribute("style", "color:red;");
+        }
+    }
+    else{ 
+        return
+    };
+    
+}
